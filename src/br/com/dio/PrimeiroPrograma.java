f75faddb7eb11de26dff473391fe6c39f5854a46
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-02-21 12:08:52 -0300

    Criando classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -10,13 +10,47 @@ public class PrimeiroPrograma {
         gato.setNome("Alfredo");
         System.out.println(gato);
 
-        Gato gato2 = new Gato();
-        gato2.setNome("Jose");
-        gato2.setCor("Azul");
-        gato2.setIdade(15);
+        Gato gato2 = new Gato("rapha","Azul",20);
         System.out.println(gato2);
+
+        Livro livro1 = new Livro("Pense como um gato", 300);
+        System.out.println(livro1);
         /*int a = 5;
         int b = 4;
         System.out.println("A soma e: " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer NumPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        NumPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return NumPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        NumPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", NumPaginas=" + NumPaginas +
+                '}';
     }
 }
